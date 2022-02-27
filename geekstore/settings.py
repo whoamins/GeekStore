cd1eb179659587c4e207f043179bb389f787955b
whoamins <40am1ns@github.com>
2022-02-28 01:00:18 +0300

    User model

--- geekstore/settings.py
+++ geekstore/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products.apps.ProductsConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from users.models import User
+
+
+# TODO: Display user fields in admin panel
+
+
+admin.site.register(User)
