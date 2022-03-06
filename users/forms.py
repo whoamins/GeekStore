5c235946af97d7f0432cf924efae55fa78a32312
whoamins <40am1ns@github.com>
2022-03-06 14:31:03 +0300

    user authentication form

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():   # WTF
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}),
+        min_length=1, max_length=50)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        min_length=1, max_length=128)
 
     class Meta:
         model = User
         fields = ('username', 'password')
+
